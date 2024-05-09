73c6515eeb2e1dafcf0d6b999d25964ab896cf34
Luis Espinola <lespinola@gmail.com>
2024-05-09 16:49:39 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <main>
             <Component {...pageProps} />
+            <GoogleAnalytics gaId='G-SHJ2MBRLFT'/>
         </main>
     );
 }
